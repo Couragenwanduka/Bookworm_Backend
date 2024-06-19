64de763203368fac23bb8fb0d292d5f18251778e
couragenwanduka <courageobunike@gmail.com>
2024-06-19 22:35:49 +0100

    making progress

--- src/helper/getText.ts
+++ src/helper/getText.ts
@@ -1,35 +1,43 @@
-import fetch from 'node-fetch';
+import superagent from 'superagent';
 import * as cheerio from 'cheerio';
 
-export const fetchHTML = async (url: string, retries = 3, delay = 3000): Promise<string | null> => {
-    try {
-        const timeout :any = 10000
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Failed to fetch HTML content. Status code: ${response.status}`);
-        }
+const MAX_RETRIES = 3;
+const INITIAL_DELAY_MS = 3000;
+const TIMEOUT_MS = 60000; // 60 seconds
 
-        const html = await response.text();
+export const fetchHTML = async (url: string, retries = MAX_RETRIES, delay = INITIAL_DELAY_MS): Promise<string | null> => {
+    try {
+        const response = await superagent.get(url).timeout({ response: TIMEOUT_MS }); // Increased timeout to 60 seconds
+
+        const html = response.text;
         if (!html) {
             throw new Error('Received empty HTML content');
         }
 
         const $ = cheerio.load(html);
         const text = $('body').text().trim();
+
         if (!text) {
             throw new Error('Failed to extract text from HTML content');
         }
 
         return text;
     } catch (error: any) {
-        if (retries > 0) {
-            console.error(`Error fetching HTML (will retry in ${delay / 1000} seconds):`, error.message);
+        console.error('Error:', error);
+
+        if (error.code === 'ETIMEDOUT' && retries > 0) {
+            console.error(`Request timed out (will retry in ${delay / 1000} seconds):`, error.message);
             await new Promise(resolve => setTimeout(resolve, delay));
-            return fetchHTML(url, retries - 1, delay);
+            return fetchHTML(url, retries - 1, delay * 2); // Exponential backoff
+        } else if (error.code === 'ENETUNREACH' && retries > 0) {
+            console.error('Network unreachable:', error.message);
+            await new Promise(resolve => setTimeout(resolve, delay));
+            return fetchHTML(url, retries - 1, delay * 2); // Exponential backoff
         } else {
             console.error('Error fetching HTML (no more retries):', error.message);
             return null;
         }
     }
+    return null; // Return null if all retries fail or for other errors
 };
 
